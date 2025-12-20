fdb23e0736d476d14c155c8c6a36e84e3e007479
mydevelopmentanant <mydevelopment.anant@gmail.com>
2025-12-20 08:18:02 -0800

    Log Changes

--- src/CommonArchitecture.Web/obj/Debug/net9.0/CommonArchitecture.Web.AssemblyInfo.cs
+++ src/CommonArchitecture.Web/obj/Debug/net9.0/CommonArchitecture.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonArchitecture.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5051565fbd0ecdff96a095f8651870dd7337b603")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ec7985e5d62e1601008cfd55b9b2b0c34deb108")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonArchitecture.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommonArchitecture.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
